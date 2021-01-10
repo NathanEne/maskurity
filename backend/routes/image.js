8a68e857d299b90051e993398300d67bc2c375df
Nathan <nathan@enerson.ca>
2021-01-09 22:30:20 -0700

    married the front and back end like the happy couple they are

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const Router = require("express-promise-router");
-
+const projectId = process.env.PROJECT_ID;
+const location = process.env.LOCATION;
+//TODO: Add model ID after training is complete
+const modelId = process.env.MODEL_ID;
+const { PredictionServiceClient } = require("@google-cloud/automl").v1;
+const client = new PredictionServiceClient();
 const router = Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { imgString } = req.body; // data:image/jpeg;base64, ...
-  console.log(imgString);
 
   let split;
 
@@ -14,11 +19,31 @@ router.post("/", (req, res) => {
     res.status(400).json({ message: "Bad request" });
   }
 
-  console.log(split);
-
-  // send image to google for processing
-
-  res.json({ message: "HI" });
+  let result = await predict(Buffer.from(split[2], "base64"));
+  console.log(result);
+  res.json(result);
 });
 
+async function predict(imgBuffer) {
+  const request = {
+    name: client.modelPath(projectId, location, modelId),
+    payload: {
+      image: {
+        imageBytes: imgBuffer,
+      },
+    },
+  };
+  const [response] = await client.predict(request);
+
+  for (const annotationPayload of response.payload) {
+    console.log(`Predicted class name: ${annotationPayload.displayName}`);
+    console.log(
+      `Predicted class score: ${annotationPayload.classification.score}`
+    );
+    return {
+      name: annotationPayload.displayName,
+      score: annotationPayload.classification.score,
+    };
+  }
+}
 module.exports = router;
