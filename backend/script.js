856a4ce546249af8bfe5554b11ef74f9845aff4c
Nathan <nathan@enerson.ca>
2021-01-09 18:32:16 -0700

    for real this time

--- backend/script.js
+++ backend/script.js
@@ -1,19 +1,16 @@
-const projectId = 'maskurity-301220';
-const location = 'us-central1';
-
+require("dotenv").config();
+const projectId = process.env.PROJECT_ID;
+const location = process.env.LOCATION;
 //TODO: Add model ID after training is complete
-//const modelId = 'YOUR_MODEL_ID';
-
+const modelId = process.env.MODEL_ID;
 const filePath = __dirName + "/resources/img.jpg";
-const {PredictionServiceClient} = require('@google-cloud/automl').v1;
-const fs = require('fs');
 
+const { PredictionServiceClient } = require("@google-cloud/automl").v1;
+const fs = require("fs");
 const client = new PredictionServiceClient();
-
 const content = fs.readFileSync(filePath);
 
 async function predict() {
-
   const request = {
     name: client.modelPath(projectId, location, modelId),
     payload: {
